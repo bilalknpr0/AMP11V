8758315abef7922a162e1136086b96cedf97df6c
Bilal <91198296+bilalknpr0@users.noreply.github.com>
2022-03-21 22:48:41 +0300

    .
    .

---
+++ Models/Flower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sınavdeneme.Models
+{
+    public class Flower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category category { get; set; }
+
+
+    }
+}
